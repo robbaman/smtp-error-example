8e0a26395d98f8131d4baba44ea7f4f8283ed695
Robert Mulder <mulder@creativeid.nl>
2016-12-21 09:19:04 +0100

    Add project files.

---
+++ SmtpException/Controllers/TestController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace SmtpException.Controllers {
+	public class TestController : ApiController {
+
+		[HttpGet, Route("test")]
+		public async Task Test() {
+			using (var client = new SmtpClient()) {
+				var message = new MailMessage();
+				message.To.Add("test@example.com");
+				message.Body = "haha";
+				message.Subject = "ohhh";
+				await client.SendMailAsync(message);
+			}
+		}
+
+		[HttpGet, Route("test2")]
+		public async Task Test2() {
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ SmtpException/Startup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Owin;
+
+namespace SmtpException {
+	public class Startup {
+		public void Configuration(IAppBuilder app) {
+			app.Use(async (context, next) => {
+				await next();
+				// Properly getting here with statuscode 500 when exception happens
+				var x = context.Response.StatusCode;
+			});
+
+			var config = new HttpConfiguration();
+			config.MapHttpAttributeRoutes();
+			app.UseWebApi(config);
+
+		}
+	}
+}
